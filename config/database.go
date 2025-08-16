2a1afa788d3067a550fd70d6dc992f9bb46e01f2
rakamiracle <rakasabri6@gmail.com>
2025-08-17 01:32:38 +0700

    change db not use orm

--- config/database.go
+++ config/database.go
@@ -1,47 +1,38 @@
 package config
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"os"
 
+	"github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
-
-	"github.com/username/webiva-backend/models"
 )
 
-var DB *gorm.DB
+var DB *sql.DB
 
 func InitDB() {
 	_ = godotenv.Load()
 
 	dsn := fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=Asia/Jakarta",
-		os.Getenv("DB_HOST"),
+		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
+		os.Getenv("DB_HOST"),
 		os.Getenv("DB_PORT"),
-		os.Getenv("DB_SSLMODE"),
+		os.Getenv("DB_NAME"),
 	)
 
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db, err := sql.Open("mysql", dsn)
 	if err != nil {
-		log.Fatal("failed connect DB:", err)
+		log.Fatal("DB connection error:", err)
 	}
-	DB = db
 
-	// Auto migrate semua model
-	if err := DB.AutoMigrate(
-		&models.User{},
-		&models.Category{},
-		&models.Product{},
-		&models.Order{},
-		&models.OrderItem{},
-		&models.StoreSetting{},
-	); err != nil {
-		log.Fatal("failed migrate:", err)
+	if err := db.Ping(); err != nil {	
+		log.Fatal("DB not reachable:", err)
 	}
+
+	DB = db
+	log.Println("DB connected successfully")
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.23.0
 toolchain go1.24.6
 
 require (
+	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/bytedance/sonic v1.13.3 // indirect
 	github.com/bytedance/sonic/loader v0.2.4 // indirect
 	github.com/cloudwego/base64x v0.1.5 // indirect
@@ -15,6 +16,7 @@ require (
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.26.0 // indirect
+	github.com/go-sql-driver/mysql v1.9.3 // indirect
 	github.com/goccy/go-json v0.10.5 // indirect
 	github.com/golang-jwt/jwt/v5 v5.3.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
