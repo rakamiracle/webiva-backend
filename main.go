5ceccb369c9c3cfddd4c623e4bc274016bb308ce
rakamiracle <rakasabri6@gmail.com>
2025-08-23 09:11:31 +0700

    improve entry point

--- main.go
+++ main.go
@@ -17,14 +17,7 @@ func main() {
 	config.InitDB()
 
 	r := gin.Default()
-
-	// CORS: izinkan FE Nuxt saat dev (3000)
-	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000", "http://127.0.0.1:3000"},
-		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowHeaders:     []string{"Authorization", "Content-Type"},
-		AllowCredentials: true,
-	}))
+	r.Use(cors.Default())
 
 	routes.Register(r)
 
@@ -32,7 +25,5 @@ func main() {
 	if port == "" { port = "8080" }
 
 	log.Println("server running on :" + port)
-	if err := r.Run(":" + port); err != nil {
-		log.Fatal(err)
-	}
+	r.Run(":" + port)
 }
