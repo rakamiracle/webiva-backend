217c7681ca38c383460af65788f47ce58badeb0d
rakamiracle <rakasabri6@gmail.com>
2025-08-24 22:08:52 +0700

    Improve hash.go from utils

--- utils/hash.go
+++ utils/hash.go
@@ -3,7 +3,7 @@ package utils
 import "golang.org/x/crypto/bcrypt"
 
 func HashPassword(p string) (string, error) {
-	b, err := bcrypt.GenerateFromPassword([]byte(p), bcrypt.DefaultCost)
+	bytes, err := bcrypt.GenerateFromPassword([]byte(p), bcrypt.DefaultCost)
 	return string(bytes), err
 }
 
