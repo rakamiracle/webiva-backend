08cdba82a019e962a282e7316158e05a0ab02aba
rakamiracle <rakasabri6@gmail.com>
2025-09-09 20:34:12 +0700

    upload fitur

---
+++ controllers/upload.go
@@ -0,0 +1,28 @@
+package controllers
+
+import (
+	"net/http"
+	"path/filepath"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func UploadFile(c *gin.Context) {
+	file, err := c.FormFile("file")
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "file required"})
+		return
+	}
+
+	// bikin nama unik
+	filename := time.Now().Format("20060102150405") + "_" + filepath.Base(file.Filename)
+	path := "uploads/" + filename
+
+	if err := c.SaveUploadedFile(file, path); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"url": "/" + path})
+}
